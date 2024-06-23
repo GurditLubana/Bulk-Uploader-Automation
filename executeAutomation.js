7a0e4674bb6581410b83c2b4cdd92ca758e70845
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-06-22 22:16:00 -0500

    removed spaces and comments, so that doc could be converted to a single line

--- executeAutomation.js
+++ executeAutomation.js
@@ -3,27 +3,16 @@ javascript:(
         function extractPolicyNumber(fileName) {
             return (fileName.split(' ')[0]).trim();
         }
-
         var tableBody = document.querySelector('.k-grid-content .k-selectable tbody');
-
 if (tableBody) {
-    
     var rows = tableBody.querySelectorAll('tr');
     rows.forEach(function(row) {
-        
         var docName = (row.children[2]).innerText;
-        
-        // console.log(docName);
         var policyNum = (docName.split(" ")[0]).trim();
         row.children[3].innerText = policyNum;
         row.children[4].innerHTML = '<span class="k-dirty"></span>Notices and Statements';
         row.children[5].innerHTML = '<span class="k-dirty"></span>Statement';
-        row.children[6].innerText = 'Return Mail';
-        // console.log(policyNum);
-        
-        })}
-    }
-)();
+        row.children[6].innerText = 'Return Mail';})}})();
 
 
 
