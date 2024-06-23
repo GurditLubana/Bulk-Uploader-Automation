30112abb1f1e6d35f77eae4acc242a708d0e6167
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-06-22 22:04:09 -0500

    converted js into bookmarklet

--- executeAutomation.js
+++ executeAutomation.js
@@ -1,18 +1,36 @@
-var tableBody = document.querySelector('.k-grid-content .k-selectable tbody');
+javascript:(
+    function() {
+        function extractPolicyNumber(fileName) {
+            return (fileName.split(' ')[0]).trim();
+        }
 
-// Check if tableBody is found
+        var tableBody = document.querySelector('.k-grid-content .k-selectable tbody');
+
 if (tableBody) {
-    // Select all rows within the table body
+    
     var rows = tableBody.querySelectorAll('tr');
-
-    // Loop through each row
     rows.forEach(function(row) {
-        // Log the row (or perform any action)
+        
         var docName = (row.children[2]).innerText;
         
         // console.log(docName);
-        var policyNum = (docName.split(" ")[0]).trim();
-        console.log(policyNum);
+        var policyNum = extractPolicyNumber(docName)
+        row.children[3].innerText = policyNum;
+        row.children[4].innerHTML = '<span class="k-dirty"></span>Notices and Statements';
+        row.children[5].innerHTML = '<span class="k-dirty"></span>Statement';
+        row.children[6].innerText = 'Return Mail';
+        // console.log(policyNum);
         
-        })
-                 }
+        })}
+    }
+)();
+
+
+
+
+
+
+
+
+
+
